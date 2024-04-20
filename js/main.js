7a7e8d7fbf33bc4abc905bb4f4ab36923eaa9b94
manolo0o <147361804+manolo0o@users.noreply.github.com>
2024-04-19 21:00:04 -0500

    feat: :rocket: main file

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import {contrasenaValida} from './modulo/contrasenaValida.js'
-import prompt from 'async-prompt'
-
-let contraseña = await prompt ("Ingrese la contraseña: ");
-
-console.log(contrasenaValida(contraseña))
--- js/modulo/contraseñaValida.js
+++
@@ -1,6 +0,0 @@
-export const contrasenaValida = (contraseña) => {
-    if (contraseña == "2Fj(jjbFsuj" || contraseña == "eoZiugBf&g9"){
-        return true;
-    }
-    return false;
-}
