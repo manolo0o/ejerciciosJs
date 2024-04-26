f0341f4d3a545bb2c882da787423a74077e7def2
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-04-26 10:26:59 -0500

    feat: :white_check_mark: Realizacion de consulta #16.
    none at moment.

--- js/main.js
+++ js/main.js
@@ -3,24 +3,29 @@ import prompt from 'async-prompt'
 //import { 
 //    getAllFullNameAndEmailsAndBoss,
 //    getBossFullNameAndEmail, getAllFullNamePositionDiferentSalesRepresentative
-//} from "./module/employees.js";
+//} 
+//from "./module/employees.js";
 
 // console.log(await getAllFullNameAndEmailsAndBoss())
 //console.log(await getBossFullNameAndEmail());
 //console.log(await getAllFullNamePositionDiferentSalesRepresentative())
 
-//import { getAllClientsfromSpain} from "./module/clients.js";
-//console.log(await getAllClientsfromSpain())
+import { getAllClientsfromSpain, getClients_From_Madrid} 
+from "./module/clients.js";
+console.log(await getClients_From_Madrid())
 
 
 //import { getAllStatus, getAllOrderCodeClientCodeAndOrdersThatHaveNotBeenDeliveredOnTime,
 //        getAllOrderCodeClientCodeAndDeliveryDateLeastTwoDaysBefore, getAllRefusedRequest_In2009,
-//        getAll_Request_From_January  } from "./module/request.js";
+//        getAll_Request_From_January  } 
+//from "./module/request.js";
 //console.log(await getAll_Request_From_January ());
 
 
-//import { getClientPayments_At_2008, getAll_Type_Of_Payments} from "./module/payments.js"
+//import { getClientPayments_At_2008, getAll_Type_Of_Payments} 
+//from "./module/payments.js"
 //console.log(await getAll_Type_Of_Payments())
 
-import {getAll_OrnamentalProducts} from "./module/product.js"
-console.log(await getAll_OrnamentalProducts())
+//import {getAll_OrnamentalProducts} 
+//from "./module/product.js"
+//console.log(await getAll_OrnamentalProducts())
--- js/module/clients.js
+++ js/module/clients.js
@@ -1,3 +1,5 @@
+//------------------------------------------------------------------------------
+
 //6.Devuelve un listado con el nombre de los todos los clientes españoles.
 export const getAllClientsfromSpain = async () => {
     let res = await fetch("http://localhost:5501/clients?country=Spain")
@@ -11,3 +13,17 @@ export const getAllClientsfromSpain = async () => {
     return dataUpdate;
 }
 
+//------------------------------------------------------------------------------
+
+// 16. Devuelve un listado con todos los clientes que sean de la 
+// ciudad de Madrid y cuyo representante de ventas tenga el código 
+// de empleado 11 o 30.
+
+
+export const getClients_From_Madrid = async () => {
+    let res = await fetch ("http://localhost:5501/clients?city=Madrid")
+    let data = await res.json();
+    let clientUpdate = [];
+    clientUpdate = data.filter(val => val.code_employee_sales_manager == 11 || val.code_employee_sales_manager == 30);
+    return clientUpdate
+}
